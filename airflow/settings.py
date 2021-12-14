8a5aa3039d779385338c452ed1221eb209ed1b1b
G'anisher <git@ganisher.kz>
2021-12-14 11:33:12 +0600

    Add Faq model

--- airflow/settings.py
+++ airflow/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'airflow_app'
 ]
 
 MIDDLEWARE = [
--- airflow_app/models.py
+++ airflow_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Faq(models.Model):
+    question = models.TextField()
+    answer = models.TextField()
+    idx = models.PositiveSmallIntegerField()
